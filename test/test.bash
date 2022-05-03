fdf6ed01b67ff8d363e83df6b61d3cc13c5c35a6
marianalevineu <m.levi@northeastern.edu>
2022-05-03 11:38:47 -0400

    added test example

---
+++ test/test.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=test-parallel
+#SBATCH --output=%j.out
+#SBATCH --error=%j.err
+#SBATCH --time=01:00:00
+#SBATCH --ntasks=2 
+#SBATCH --mem=20GB
+#SBATCH --partition=short
+#SBATCH --constraint=cascadelake ##use only new nodes, with IB network support
+
+source $HOME/opensees/setenv_opensees.sh
+
+mpirun -n 2 OpenSeesMP example4.tcl 
